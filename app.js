875176237b60306ce60d5cb1da6c30aec28d4f37
a-sayuo <21yuoyuoyuo@gmail.com>
2025-10-21 04:54:16 +0900

    CSS

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("form.ejs");
+});
+
+app.listen(3000);
